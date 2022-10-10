6aac488348c1706ce4da2bdff2bc2bc0544868dd
George Kouros <george.kouros.ece@gmail.com>
2022-10-10 09:55:49 +0200

    Adjust jobs

--- condor/train.sh
+++ condor/train.sh
@@ -14,12 +14,19 @@ export LD_LIBRARY_PATH="/usr/local/cuda-11/lib64:/usr/local/cuda/lib64:$CONDA_PR
 DIR=/users/visics/gkouros/projects/nerf-repos/Ref-NeRF-plusplus/
 cd ${DIR}
 
+ENABLE_PRED_ROUGHNESS=True
+DEG_VIEW=7
+BATCH_SIZE=1024
+RENDER_CHUNK_SIZE=1024
+
 XLA_PYTHON_CLIENT_ALLOCATOR=platform TF_FORCE_GPU_ALLOW_GROWTH='true' python3 train.py \
   --gin_configs=$CONFIG \
   --gin_bindings="Config.data_dir = '${DIR}/data/$1'" \
   --gin_bindings="Config.checkpoint_dir = '${DIR}/logs/$1/$2'" \
-  --gin_bindings="NerfMLP.deg_view = 5" \
-  --gin_bindings="NerfMLP.enable_pred_roughness = False" \
+  --gin_bindings="Config.batch_size = $BATCH_SIZE" \
+  --gin_bindings="Config.render_chunk_size = $RENDER_CHUNK_SIZE" \
+  --gin_bindings="NerfMLP.deg_view = $DEG_VIEW" \
+  --gin_bindings="NerfMLP.enable_pred_roughness = $ENABLE_PRED_ROUGHNESS" \
   --logtostderr \
   && \
   python3 render.py \
@@ -27,9 +34,15 @@ XLA_PYTHON_CLIENT_ALLOCATOR=platform TF_FORCE_GPU_ALLOW_GROWTH='true' python3 tr
     --gin_bindings="Config.data_dir = '${DATA_DIR}'" \
     --gin_bindings="Config.checkpoint_dir = '${DIR}/logs/$1/$2'" \
     --gin_bindings="Config.render_dir = '${DIR}/logs/$1/$2/render/'" \
-    --gin_bindings="Config.render_path = True" \
+    --gin_bindings="Config.render_path = False" \
     --gin_bindings="Config.render_path_frames = 480" \
     --gin_bindings="Config.render_video_fps = 60" \
-    --logtostderr
+    --gin_bindings="Config.batch_size = $BATCH_SIZE" \
+    --gin_bindings="Config.render_chunk_size = $RENDER_CHUNK_SIZE"
+    --gin_bindings="NerfMLP.deg_view = $DEG_VIEW" \
+    --gin_bindings="NerfMLP.enable_pred_roughness = $ENABLE_PRED_ROUGHNESS" \
+    --logtostderr \
+  && \
+
 
 conda deactivate
