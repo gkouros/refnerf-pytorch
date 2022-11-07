b5d1bd124e4bc21788fcf8539f5de8dc23d89714
George Kouros <george.kouros.ece@gmail.com>
2022-11-07 17:24:09 +0100

    Port train script to pytorch

--- train.py
+++ train.py
@@ -35,34 +35,36 @@ TIME_PRECISION = 1000  # Internally represent integer times in milliseconds.
 
 
 def main(unused_argv):
-    np.random.seed(20201473)
     config = configs.load_config()
-    
+
     # load training and test sets
     dataset = datasets.load_dataset('train', config.data_dir, config)
     test_dataset = datasets.load_dataset('test', config.data_dir, config)
-    
+
     # create model, state, rendering evaluation function, training step, and lr scheduler
     setup = train_utils.setup_model(config, dataset=dataset)
-    model, state, render_eval_pfn, train_pstep, lr_fn = setup
-
-    variables = state.params
+    model, variables, optimizer, lr_scheduler, render_eval_fn, train_step = setup
+    state = {
+        'step': 1,
+        'model': model.state_dict(),
+        'optim': optimizer.state_dict(),
+        'lr_scheduler': lr_scheduler.state_dict(),
+    }
     num_params = np.prod(variables.shape)
     print(f'Number of parameters being optimized: {num_params}')
-
-    if (dataset.size > model.num_glo_embeddings and model.num_glo_features > 0):
-        raise ValueError(f'Number of glo embeddings {model.num_glo_embeddings} '
-                         f'must be at least equal to number of train images '
-                         f'{dataset.size}')
 
     # create object for calculating metrics
     metric_harness = image.MetricHarness()
 
-    # load saved checkpoint
-    if not utils.isdir(config.checkpoint_dir):
+    # load saved checkpoint or create checkpoint dir if not there
+    if utils.isdir(config.checkpoint_dir):
+        state = torch.load(config.checkpoint_dir)
+        model.load_state_dict(state['model'])
+        optimizer.load_state_dict(state['optimizer'])
+        lr_scheduler.load_state_dict(state['lr_scheduler'])
+    else:
         utils.makedirs(config.checkpoint_dir)
-    # state = checkpoints.restore_checkpoint(config.checkpoint_dir, state)
-    state = torch.load(config.checkpoint_dir)
+
     # Resume training at the step of the last checkpoint.
     init_step = state.step + 1
 
@@ -70,7 +72,7 @@ def main(unused_argv):
     summary_writer = tensorboard.SummaryWriter(config.checkpoint_dir)
 
     # Prefetch_buffer_size = 3 x batch_size.
-    gc.disable()  # Disable automatic garbage collection for efficiency.
+    # gc.disable()  # Disable automatic garbage collection for efficiency.
     total_time = 0
     total_steps = 0
     reset_stats = True
@@ -78,6 +80,9 @@ def main(unused_argv):
         num_steps = config.early_exit_steps
     else:
         num_steps = config.max_steps
+
+    # set model to training mode
+    model.train()
 
     # start training loop
     for step, batch in zip(range(init_step, num_steps + 1), dataset):
@@ -87,12 +92,14 @@ def main(unused_argv):
             train_start_time = time.time()
             reset_stats = False
 
-        learning_rate = lr_fn(step)
-        train_frac = jnp.clip((step - 1) / (config.max_steps - 1), 0, 1)
+        # update fraction of completed training
+        train_frac = np.clip((step - 1) / (config.max_steps - 1), 0, 1)
 
         # perform training step
-        state, stats = train_pstep(
-            state,
+        stats = train_step(
+            model,
+            optimizer,
+            lr_scheduler,
             batch,
             dataset.cameras,
             train_frac,
@@ -105,117 +112,132 @@ def main(unused_argv):
         # Log training summaries
         stats_buffer.append(stats)
 
-        if step == init_step or step % config.print_every == 0:
-            elapsed_time = time.time() - train_start_time
-            steps_per_sec = config.print_every / elapsed_time
-            rays_per_sec = config.batch_size * steps_per_sec
+        with torch.no_grad():
 
-            # A robust approximation of total training time, in case of pre-emption.
-            total_time += int(round(TIME_PRECISION * elapsed_time))
-            total_steps += config.print_every
-            approx_total_time = int(round(step * total_time / total_steps))
+            if step == init_step or step % config.print_every == 0:
 
-            # Transpose and stack stats_buffer along axis 0.
-            # fs = [flax.traverse_util.flatten_dict(s, sep='/') for s in stats_buffer]  #TODO:
-            fs = stats_buffer
-            stats_stacked = {k: np.stack([f[k] for f in fs]) for k in fs[0].keys()}
+                elapsed_time = time.time() - train_start_time
+                steps_per_sec = config.print_every / elapsed_time
+                rays_per_sec = config.batch_size * steps_per_sec
 
-            # Split every statistic that isn't a vector into a set of statistics.
-            stats_split = {}
-            for k, v in stats_stacked.items():
-                if v.ndim not in [1, 2] and v.shape[0] != len(stats_buffer):
-                    raise ValueError('statistics must be of size [n], or [n, k].')
-                if v.ndim == 1:
-                    stats_split[k] = v
-                elif v.ndim == 2:
-                    for i, vi in enumerate(tuple(v.T)):
-                        stats_split[f'{k}/{i}'] = vi
+                # A robust approximation of total training time, in case of pre-emption.
+                total_time += int(round(TIME_PRECISION * elapsed_time))
+                total_steps += config.print_every
+                approx_total_time = int(round(step * total_time / total_steps))
 
-            # Summarize the entire histogram of each statistic.
-            for k, v in stats_split.items():
-                summary_writer.histogram('train_' + k, v, step)
+                # Transpose and stack stats_buffer along axis 0.
+                # fs = [flax.traverse_util.flatten_dict(s, sep='/') for s in stats_buffer]  #TODO:
+                fs = stats_buffer
+                stats_stacked = {k: np.stack([f[k] for f in fs]) for k in fs[0].keys()}
 
-            # Take the mean and max of each statistic since the last summary.
-            avg_stats = {k: jnp.mean(v) for k, v in stats_split.items()}
-            max_stats = {k: jnp.max(v) for k, v in stats_split.items()}
+                # Split every statistic that isn't a vector into a set of statistics.
+                stats_split = {}
+                for k, v in stats_stacked.items():
+                    if v.ndim not in [1, 2] and v.shape[0] != len(stats_buffer):
+                        raise ValueError('statistics must be of size [n], or [n, k].')
+                    if v.ndim == 1:
+                        stats_split[k] = v
+                    elif v.ndim == 2:
+                        for i, vi in enumerate(tuple(v.T)):
+                            stats_split[f'{k}/{i}'] = vi
 
-            # Summarize the mean and max of each statistic.
-            for k, v in avg_stats.items():
-                summary_writer.scalar(f'train_avg_{k}', v, step)
-            for k, v in max_stats.items():
-                summary_writer.scalar(f'train_max_{k}', v, step)
+                # Summarize the entire histogram of each statistic.
+                for k, v in stats_split.items():
+                    summary_writer.histogram('train_' + k, v, step)
 
-            summary_writer.scalar('train_num_params', num_params, step)
-            summary_writer.scalar('train_learning_rate', learning_rate, step)
-            summary_writer.scalar('train_steps_per_sec', steps_per_sec, step)
-            summary_writer.scalar('train_rays_per_sec', rays_per_sec, step)
+                # Take the mean and max of each statistic since the last summary.
+                avg_stats = {k: jnp.mean(v) for k, v in stats_split.items()}
+                max_stats = {k: jnp.max(v) for k, v in stats_split.items()}
 
-            summary_writer.scalar('train_avg_psnr_timed', avg_stats['psnr'],
-                                  total_time // TIME_PRECISION)
-            summary_writer.scalar('train_avg_psnr_timed_approx', avg_stats['psnr'],
-                                  approx_total_time // TIME_PRECISION)
-            precision = int(np.ceil(np.log10(config.max_steps))) + 1
-            avg_loss = avg_stats['loss']
-            avg_psnr = avg_stats['psnr']
-            str_losses = {  # Grab each "losses_{x}" field and print it as "x[:4]".
-                k[7:11]: (f'{v:0.5f}' if v >= 1e-4 and v < 10 else f'{v:0.1e}')
-                for k, v in avg_stats.items()
-                if k.startswith('losses/')
-            }
-            print(f'{step:{precision}d}' + f'/{config.max_steps:d}: ' +
-                  f'loss={avg_loss:0.5f}, ' + f'psnr={avg_psnr:6.3f}, ' +
-                  f'lr={learning_rate:0.2e} | ' +
-                  ', '.join([f'{k}={s}' for k, s in str_losses.items()]) +
-                  f', {rays_per_sec:0.0f} r/s')
+                # Summarize the mean and max of each statistic.
+                for k, v in avg_stats.items():
+                    summary_writer.scalar(f'train_avg_{k}', v, step)
+                for k, v in max_stats.items():
+                    summary_writer.scalar(f'train_max_{k}', v, step)
 
-            # Reset everything we are tracking between summarizations.
-            reset_stats = True
+                summary_writer.scalar('train_num_params', num_params, step)
+                summary_writer.scalar('train_learning_rate', learning_rate, step)
+                summary_writer.scalar('train_steps_per_sec', steps_per_sec, step)
+                summary_writer.scalar('train_rays_per_sec', rays_per_sec, step)
+                summary_writer.scalar('train_avg_psnr_timed', avg_stats['psnr'],
+                                    total_time // TIME_PRECISION)
+                summary_writer.scalar('train_avg_psnr_timed_approx', avg_stats['psnr'],
+                                    approx_total_time // TIME_PRECISION)
+                precision = int(np.ceil(np.log10(config.max_steps))) + 1
+                avg_loss = avg_stats['loss']
+                avg_psnr = avg_stats['psnr']
+                str_losses = {  # Grab each "losses_{x}" field and print it as "x[:4]".
+                    k[7:11]: (f'{v:0.5f}' if v >= 1e-4 and v < 10 else f'{v:0.1e}')
+                    for k, v in avg_stats.items()
+                    if k.startswith('losses/')
+                }
+                print(f'{step:{precision}d}' + f'/{config.max_steps:d}: ' +
+                    f'loss={avg_loss:0.5f}, ' + f'psnr={avg_psnr:6.3f}, ' +
+                    f'lr={learning_rate:0.2e} | ' +
+                    ', '.join([f'{k}={s}' for k, s in str_losses.items()]) +
+                    f', {rays_per_sec:0.0f} r/s')
 
-        if step == 1 or step % config.checkpoint_every == 0:
-            torch.save({'state': state, 'step': step}, config.checkpoint_dir)
+                # Reset everything we are tracking between summarizations.
+                reset_stats = True
 
+            # save a checkpoint on the first epoch and every Nth epoch
+            if step == 1 or step % config.checkpoint_every == 0:
+                # save checkpoint
+                torch.save({
+                    'step': step,
+                    'model': model.state_dict(),
+                    'optimizer': optimizer.state_dict(),
+                    'lr_scheduler': lr_scheduler.state_dict(),
+                }, path=os.path.join(config.checkpoint_dir, f'checkpoint_{step}'))
 
-        # Test-set evaluation.
-        if config.train_render_every > 0 and step % config.train_render_every == 0:
-            # We reuse the same random number generator from the optimization step
-            # here on purpose so that the visualization matches what happened in
-            # training.
-            eval_start_time = time.time()
-            eval_variables = state.params
-            test_case = next(test_dataset)
-            rendering = models.render_image(
-                functools.partial(render_eval_pfn, eval_variables, train_frac),
-                test_case.rays, config)
+            # Test-set evaluation.
+            if config.train_render_every > 0 and step % config.train_render_every == 0:
+                # We reuse the same random number generator from the optimization step
+                # here on purpose so that the visualization matches what happened in
+                # training.
+                eval_start_time = time.time()
+                eval_variables = state.params
+                test_case = next(test_dataset)
+                rendering = models.render_image(
+                    functools.partial(render_eval_fn, eval_variables, train_frac),
+                    test_case.rays, config)
 
-            # Log eval summaries
-            eval_time = time.time() - eval_start_time
-            num_rays = np.prod(np.array(test_case.rays.directions.shape[:-1]))
-            rays_per_sec = num_rays / eval_time
-            summary_writer.scalar('test_rays_per_sec', rays_per_sec, step)
-            print(f'Eval {step}: {eval_time:0.3f}s., {rays_per_sec:0.0f} rays/sec')
+                # Log eval summaries
+                eval_time = time.time() - eval_start_time
+                num_rays = np.prod(np.array(test_case.rays.directions.shape[:-1]))
+                rays_per_sec = num_rays / eval_time
+                summary_writer.scalar('test_rays_per_sec', rays_per_sec, step)
+                print(f'Eval {step}: {eval_time:0.3f}s., {rays_per_sec:0.0f} rays/sec')
 
-            if config.compute_eval_metrics:
-                metric_start_time = time.time()
-                metric = metric_harness(rendering['rgb'], test_case.rgb)
-                print(f'Metrics computed in {(time.time() - metric_start_time):0.3f}s')
-                for name, val in metric.items():
-                    if not np.isnan(val):
-                        print(f'{name} = {val:.4f}')
-                        summary_writer.scalar(
-                            'train_metrics/' + name, val, step)
+                if config.compute_eval_metrics:
+                    metric_start_time = time.time()
+                    metric = metric_harness(rendering['rgb'], test_case.rgb)
+                    print(f'Metrics computed in {(time.time() - metric_start_time):0.3f}s')
+                    for name, val in metric.items():
+                        if not np.isnan(val):
+                            print(f'{name} = {val:.4f}')
+                            summary_writer.scalar(
+                                'train_metrics/' + name, val, step)
 
-            vis_start_time = time.time()
-            vis_suite = vis.visualize_suite(rendering, test_case.rays)
-            print(f'Visualized in {(time.time() - vis_start_time):0.3f}s')
-            summary_writer.image('test_true_color', test_case.rgb, step)
-            if config.compute_normal_metrics:
-                summary_writer.image('test_true_normals',
-                                     test_case.normals / 2. + 0.5, step)
-            for k, v in vis_suite.items():
-                summary_writer.image('test_output_' + k, v, step)
+                vis_start_time = time.time()
+                vis_suite = vis.visualize_suite(rendering, test_case.rays)
+                print(f'Visualized in {(time.time() - vis_start_time):0.3f}s')
+                summary_writer.image('test_true_color', test_case.rgb, step)
+                if config.compute_normal_metrics:
+                    summary_writer.image('test_true_normals',
+                                        test_case.normals / 2. + 0.5, step)
+                for k, v in vis_suite.items():
+                    summary_writer.image('test_output_' + k, v, step)
 
-    if config.max_steps % config.checkpoint_every != 0:
-        torch.save({'state': state, 'step': int(config.max_steps)}, config.checkpoint_dir)
+        # save last checkpoint if it wasn't already saved
+        if config.max_steps % config.checkpoint_every != 0:
+            # TODO: use pytorch based checkpointing
+                torch.save({
+                    'step': config.max_steps,
+                    'model': model.state_dict(),
+                    'optimizer': optimizer.state_dict(),
+                    'lr_scheduler': lr_scheduler.state_dict(),
+                }, path=os.path.join(config.checkpoint_dir, f'checkpoint_{config.max_steps}'))
 
 
 if __name__ == '__main__':
