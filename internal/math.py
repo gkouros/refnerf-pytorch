9a8fc7cefde0518e8345893374bd3d3275e93c98
George Kouros <george.kouros.ece@gmail.com>
2022-11-15 13:42:44 +0100

    Add 1-d interpolation function and fix formatting

--- internal/math.py
+++ internal/math.py
@@ -19,49 +19,51 @@ import numpy as np
 
 
 def matmul(a, b):
-  """Helper function for matrix multiplication."""
-  return torch.matmul(a, b)
+    """Helper function for matrix multiplication."""
+    return torch.matmul(a, b)
 
 
 def safe_trig_helper(x, fn, t=100 * torch.pi):
-  """Helper function used by safe_cos/safe_sin: mods x before sin()/cos()."""
-  return fn(torch.where(torch.abs(x) < t, x, x % t))
+    """Helper function used by safe_cos/safe_sin: mods x before sin()/cos()."""
+    return fn(torch.where(torch.abs(x) < t, x, x % t))
 
 
 def safe_cos(x):
-  """torch.cos() on a TPU may NaN out for large values."""
-  return safe_trig_helper(x, torch.cos)
+    """torch.cos() on a TPU may NaN out for large values."""
+    return safe_trig_helper(x, torch.cos)
 
 
 def safe_sin(x):
-  """torch.sin() on a TPU may NaN out for large values."""
-  return safe_trig_helper(x, torch.sin)
+    """torch.sin() on a TPU may NaN out for large values."""
+    return safe_trig_helper(x, torch.sin)
 
-#TODO: needs safe Jacobian calculation. How do I do it in PyTorch?
+# TODO: needs safe Jacobian calculation. How do I do it in PyTorch?
 # Maybe this solution? https://discuss.pytorch.org/t/notimplementederror-you-must-implement-the-jvp-function-for-custom-autograd-function-to-use-it-with-forward-mode-ad/138245/2
 # @jax.custom_jvp
+
+
 def safe_exp(x):
-  """torch.exp() but with finite output and gradients for large inputs."""
-  return torch.exp(torch.minimum(x, 88.))  # torch.exp(89) is infinity.
+    """torch.exp() but with finite output and gradients for large inputs."""
+    return torch.exp(torch.minimum(x, 88.))  # torch.exp(89) is infinity.
 
 
 # @safe_exp.defjvp
 def safe_exp_jvp(primals, tangents):
-  """Override safe_exp()'s gradient so that it's large when inputs are large."""
-  x, = primals
-  x_dot, = tangents
-  exp_x = safe_exp(x)
-  exp_x_dot = exp_x * x_dot
-  return exp_x, exp_x_dot
+    """Override safe_exp()'s gradient so that it's large when inputs are large."""
+    x, = primals
+    x_dot, = tangents
+    exp_x = safe_exp(x)
+    exp_x_dot = exp_x * x_dot
+    return exp_x, exp_x_dot
 
 
 def log_lerp(t, v0, v1):
-  """Interpolate log-linearly from `v0` (t=0) to `v1` (t=1)."""
-  if v0 <= 0 or v1 <= 0:
-    raise ValueError(f'Interpolants {v0} and {v1} must be positive.')
-  lv0 = np.log(v0)
-  lv1 = np.log(v1)
-  return np.exp(np.clip(t, 0, 1) * (lv1 - lv0) + lv0)
+    """Interpolate log-linearly from `v0` (t=0) to `v1` (t=1)."""
+    if v0 <= 0 or v1 <= 0:
+        raise ValueError(f'Interpolants {v0} and {v1} must be positive.')
+    lv0 = np.log(v0)
+    lv1 = np.log(v1)
+    return np.exp(np.clip(t, 0, 1) * (lv1 - lv0) + lv0)
 
 
 def learning_rate_decay(step,
@@ -70,61 +72,91 @@ def learning_rate_decay(step,
                         max_steps,
                         lr_delay_steps=0,
                         lr_delay_mult=1):
-  """Continuous learning rate decay function.
+    """Continuous learning rate decay function.
 
-  The returned rate is lr_init when step=0 and lr_final when step=max_steps, and
-  is log-linearly interpolated elsewhere (equivalent to exponential decay).
-  If lr_delay_steps>0 then the learning rate will be scaled by some smooth
-  function of lr_delay_mult, such that the initial learning rate is
-  lr_init*lr_delay_mult at the beginning of optimization but will be eased back
-  to the normal learning rate when steps>lr_delay_steps.
+    The returned rate is lr_init when step=0 and lr_final when step=max_steps, and
+    is log-linearly interpolated elsewhere (equivalent to exponential decay).
+    If lr_delay_steps>0 then the learning rate will be scaled by some smooth
+    function of lr_delay_mult, such that the initial learning rate is
+    lr_init*lr_delay_mult at the beginning of optimization but will be eased back
+    to the normal learning rate when steps>lr_delay_steps.
 
-  Args:
-    step: int, the current optimization step.
-    lr_init: float, the initial learning rate.
-    lr_final: float, the final learning rate.
-    max_steps: int, the number of steps during optimization.
-    lr_delay_steps: int, the number of steps to delay the full learning rate.
-    lr_delay_mult: float, the multiplier on the rate when delaying it.
+    Args:
+      step: int, the current optimization step.
+      lr_init: float, the initial learning rate.
+      lr_final: float, the final learning rate.
+      max_steps: int, the number of steps during optimization.
+      lr_delay_steps: int, the number of steps to delay the full learning rate.
+      lr_delay_mult: float, the multiplier on the rate when delaying it.
 
-  Returns:
-    lr: the learning for current step 'step'.
-  """
-  if lr_delay_steps > 0:
-    # A kind of reverse cosine decay.
-    delay_rate = lr_delay_mult + (1 - lr_delay_mult) * np.sin(
-        0.5 * np.pi * np.clip(step / lr_delay_steps, 0, 1))
-  else:
-    delay_rate = 1.
-  return delay_rate * log_lerp(step / max_steps, lr_init, lr_final)
+    Returns:
+      lr: the learning for current step 'step'.
+    """
+    if lr_delay_steps > 0:
+        # A kind of reverse cosine decay.
+        delay_rate = lr_delay_mult + (1 - lr_delay_mult) * np.sin(
+            0.5 * np.pi * np.clip(step / lr_delay_steps, 0, 1))
+    else:
+        delay_rate = 1.
+    return delay_rate * log_lerp(step / max_steps, lr_init, lr_final)
 
 
 def interp(*args):
-  """A gather-based (GPU-friendly) vectorized replacement for torch.interp()."""
-  args_flat = [x.reshape([-1, x.shape[-1]]) for x in args]
-  ret = torch.vmap(torch.interp)(*args_flat).reshape(args[0].shape)
-  return ret
+    """A gather-based (GPU-friendly) vectorized replacement for torch.interp()."""
+    args_flat = [x.reshape([-1, x.shape[-1]]) for x in args]
+    ret = torch.vmap(torch.interp)(*args_flat).reshape(args[0].shape)
+    return ret
 
 
 def sorted_interp(x, xp, fp):
-  """A TPU-friendly version of interp(), where xp and fp must be sorted."""
+    """A TPU-friendly version of interp(), where xp and fp must be sorted."""
 
-  # Identify the location in `xp` that corresponds to each `x`.
-  # The final `True` index in `mask` is the start of the matching interval.
-  mask = x[..., None, :] >= xp[..., :, None]
+    # Identify the location in `xp` that corresponds to each `x`.
+    # The final `True` index in `mask` is the start of the matching interval.
+    mask = x[..., None, :] >= xp[..., :, None]
 
-  def find_interval(x):
-    # Grab the value where `mask` switches from True to False, and vice versa.
-    # This approach takes advantage of the fact that `x` is sorted.
-    # print(torch.where(mask, x[..., None], x[..., :1, None]))
-    # print(x[..., None].shape, x[..., :1, None].shape, torch.where(mask, x[..., None], x[..., :1, None]).shape)
-    x0 = torch.max(torch.where(mask, x[..., None], x[..., :1, None]), dim=-2).values
-    x1 = torch.min(torch.where(~mask, x[..., None], x[..., -1:, None]), dim=-2).values
-    return x0, x1
+    def find_interval(x):
+        # Grab the value where `mask` switches from True to False, and vice versa.
+        # This approach takes advantage of the fact that `x` is sorted.
+        # print(torch.where(mask, x[..., None], x[..., :1, None]))
+        # print(x[..., None].shape, x[..., :1, None].shape, torch.where(mask, x[..., None], x[..., :1, None]).shape)
+        x0 = torch.max(torch.where(
+            mask, x[..., None], x[..., :1, None]), dim=-2).values
+        x1 = torch.min(torch.where(
+            ~mask, x[..., None], x[..., -1:, None]), dim=-2).values
+        return x0, x1
 
-  fp0, fp1 = find_interval(fp)
-  xp0, xp1 = find_interval(xp)
+    fp0, fp1 = find_interval(fp)
+    xp0, xp1 = find_interval(xp)
 
-  offset = torch.clip(torch.nan_to_num((x - xp0) / (xp1 - xp0), 0), 0, 1)
-  ret = fp0 + offset * (fp1 - fp0)
-  return ret
+    offset = torch.clip(torch.nan_to_num((x - xp0) / (xp1 - xp0), 0), 0, 1)
+    ret = fp0 + offset * (fp1 - fp0)
+    return ret
+
+
+def interp(x: torch.Tensor, xp: torch.Tensor, fp: torch.Tensor) -> torch.Tensor:
+    """One-dimensional linear interpolation for monotonically increasing sample
+    points. Similar to np.interp.
+
+    Returns the one-dimensional piecewise linear interpolant to a function with
+    given discrete data points :math:`(xp, fp)`, evaluated at :math:`x`.
+
+    Args:
+        x: the :math:`x`-coordinates at which to evaluate the interpolated
+            values.
+        xp: the :math:`x`-coordinates of the data points, must be increasing.
+        fp: the :math:`y`-coordinates of the data points, same length as `xp`.
+
+    Returns:
+        the interpolated values, same size as `x`.
+
+    Details:
+        Taken from issue at https://github.com/pytorch/pytorch/issues/50334
+    """
+    m = (fp[1:] - fp[:-1]) / (xp[1:] - xp[:-1])
+    b = fp[:-1] - (m * xp[:-1])
+
+    indices = torch.sum(torch.ge(x[:, None], xp[None, :]), 1) - 1
+    indices = torch.clamp(indices, 0, len(m) - 1)
+
+    return m[indices] * x + b[indices]
